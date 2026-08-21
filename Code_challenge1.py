416c2a8a02638a5a5c815e559d979dfcc8a0607e
larafortuna <larajanefortuna81@gmail.com>
2026-08-21 19:02:59 +0800

    Update Code_challenge1.py

--- Code_challenge1.py
+++ Code_challenge1.py
@@ -1,3 +1,3 @@
-yolo = "\t\t\t\t*\n\t\t\t*\t\t\n\t\t*\t\n\t*\t\n*\t\t\tHi! My Name is Lara Jane Fortuna\t\t\n\t*\n\t\t*\t\t\n\t\t\t*\t\t\t\t\t\n\t\t\t\t*"
+yolo = "\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\tHI MY NAME IS\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\"LARA FORTUNA\"\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*"
 
 print(yolo)
