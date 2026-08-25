c11ef97c4f4132ae1296c70b3ef0d025b7608eae
larafortuna <larajanefortuna81@gmail.com>
2026-08-25 12:09:33 +0800

    Add files via upload
    8/25/26

---
+++ activity5.py
@@ -0,0 +1,17 @@
+a = 34
+b = 35
+sum = a + b
+diff = a - b
+product = a * b
+quotient = a / b
+floordivision  = a // b
+exponentation  = a ** b
+modulus  = a % b
+
+print("The sum of ",a,"plus",b,"is ", sum)
+print("The difference of ",a,"minus",b,"is ", diff)
+print("The product of ",a,"times",b,"is ", product)
+print("The quotient of ",a,"and",b,"is", quotient)
+print("The floordivision of ",a,"and",b,"is", floordivision)
+print("The exponentation of ",a,"and",b,"is" , exponentation)
+print("The modulus of ",a,"and",b,"is" , modulus)
