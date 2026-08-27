83a6196d1e249a0ae768d633b891c6c41d3750db
larafortuna <larajanefortuna81@gmail.com>
2026-08-27 17:47:30 +0800

    Update code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,21 +1,30 @@
 #nagugutom na po akoh
 money = 6767
 
-#what
-thousand = money//1000
-money = money - thousand*1000
-five = money//500
-money = money - five*500
-two = money//200
-money = money - two*200
-one = money//100
-sixty = money - one*60
-money = money - one*7
-money = money - ones*7
-seven = money//7
+print("Money to deposit -->", money) 
 
-print("You have ", thousand, "of 1000")
-print("You have ", five, "of 500")
-print("You have ", two, "of 200")
-print("You have ", two, "of 60")
-print("You have ", seven, "of 7")
+#Money to deposit
+thousand = money // 1000
+money = money - thousand * 1000
+
+five = money // 500
+money = money - five * 500
+
+two = money // 200
+money = money - two * 200
+
+one = money // 100
+money = money - one * 100
+
+sixty = money // 60
+money = money - sixty * 60
+
+seven = money // 7
+money = money - seven * 7
+
+print("You have", thousand, "of 1000")
+print("You have", five, "of 500")
+print("You have", two, "of 200")
+print("You have", one, "of 100")
+print("You have", sixty, "of 60")
+print("You have", seven, "of 7")
