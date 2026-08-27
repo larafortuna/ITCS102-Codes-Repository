8e54a835157a91adc631fad949b52e097bd5f6cd
larafortuna <larajanefortuna81@gmail.com>
2026-08-27 17:49:41 +0800

    Update code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -4,23 +4,23 @@ money = 6767
 print("Money to deposit -->", money) 
 
 #Money to deposit
-thousand = money // 1000
-money = money - thousand * 1000
+thousand = money//1000
+money = money - thousand*1000
 
-five = money // 500
-money = money - five * 500
+five = money//500
+money = money - five*500
 
-two = money // 200
-money = money - two * 200
+two = money//200
+money = money - two*200
 
-one = money // 100
-money = money - one * 100
+one = money//100
+money = money - one*100
 
-sixty = money // 60
-money = money - sixty * 60
+sixty = money//60
+money = money - sixty*60
 
-seven = money // 7
-money = money - seven * 7
+seven = money//7
+money = money - seven*7
 
 print("You have", thousand, "of 1000")
 print("You have", five, "of 500")
