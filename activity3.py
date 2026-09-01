251e210cd7994fa677438fc5c3e5ff49cc02309d
larafortuna <larajanefortuna81@gmail.com>
2026-09-01 12:41:01 +0800

    Add files via upload
    act3 yarn

---
+++ activity3.py
@@ -0,0 +1,23 @@
+#integer
+male = 14
+female = 25
+chair = 50
+
+#float 
+myHeight = 184.2
+temperature = 58.9
+grade = 99.9
+
+#string
+myName = "Lara Jane"
+myBb = "Jorjet Ann"
+myCat = "Gio Wow"
+
+#boolean
+isInlove = True
+isNatataehngaakoh = True
+isNotNervous = False
+
+print("Hi, my name is ", myName)
+print("Hi, my bb is ", myBb)
+print("our cat name is ", myCat)
