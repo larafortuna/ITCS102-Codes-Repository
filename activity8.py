bfaaf6ee29c97d82c0672b7675dcabfc6acc6360
larafortuna <larajanefortuna81@gmail.com>
2026-09-03 12:32:46 +0800

    Add files via upload
    baka renejay to

---
+++ activity8.py
@@ -0,0 +1,21 @@
+#concatenation = combining strings
+#mamahhhhhh
+
+hobbies = "" #itoayblank
+
+h = input("What are your hobbies --> ")
+hobbies += h + ", "
+
+h = input("wow sana all, ano pa? --> ")
+hobbies += h + ", "
+
+h = input("kakaiba yan ah :D meron pa? --> ")
+hobbies += h + ", "
+
+h = input("lopit, nasayo na lahat ah, wag mong sabihin meron pa?? --> ")
+hobbies += h + ", "
+
+h = input("anue pa..? --> ")
+hobbies += h + ", "
+
+print("So, my hobbies are ", hobbies)
