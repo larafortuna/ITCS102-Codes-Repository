34ae054ec058ee32867c16e44652178046757dc0
larafortuna <larajanefortuna81@gmail.com>
2026-09-10 18:16:31 +0800

    Update code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,30 +1,42 @@
-#KALOKETOR
+#KALKOLETOR
 
 sender = str(input("sender Name: "))
 item = str(input("item: "))
-fragile = bool(input("Fragile? "))
+fragile = input("Fragile? ") == "true"
 weight = float(input("weight of the item :"))
 distance = float(input("kilometer : "))
-is_express = bool(input("is express? "))
-is_international = bool(input("is international? "))
+is_express = input("is express? ") == "true"
+is_international = input("is international? ") == "true"
 
+#basecost
 base_cost = (weight * 2.50) + (distance * 0.15)
 
-print(base_cost)
-
-if weight <= 2.0 and distance <= 100:
-	print("Free Shipping")
-else:
-	print("with Shipping Fee")
+#bigatatlayo
+if weight <= 2.0 and distance <= 100 and not is_express and not is_international:
+    category = "Free Shipping"
+    total = 0
 
 #express
-total = (base_cost * 1.40)
-total1 = (total + 50)
+elif is_express and is_international:
+    category = "Express International"
+    total = (base_cost * 1.40) + 50
 
-#oversized
-total2 = (total1 + 30)
+elif is_express or (is_international and weight > 20):
+    category = "Express or Heavy International"
+    total = (base_cost * 1.20) + 25
 
-#standard rate
-total3 = (base_cost)
+elif weight > 30 or distance > 1000:
+    category = "Oversized"
+    total = base_cost + 30
 
+else:
+    category = "Standard Rate"
+    total = base_cost
 
+print("\n--- SHIPPING DETAILS ---")
+print("Sender:", sender)
+print("Item:", item)
+print("Weight:", weight, "kg")
+print("Distance:", distance, "km")
+print("Category:", category)
+print("Total Amount: PHP{:.2f}".format(total))
