de1036bd736978103226ab186f977f675ff6d35e
lara <larajanefortuna81@gmail.com>
2026-09-23 14:16:19 +0800

    Update activity14.py

--- activity14.py
+++ activity14.py
@@ -1,22 +1,22 @@
-# age (integer)
-# is_employed (boolean)
-# credit score (integer)
-# annual_income (float)
-# has collateral (boolean)
+#age (integer)
+#is_employed (boolean)
+#credit score (integer)
+#annual_income (float)
+#has collateral (boolean)
 
 age = int(input("Enter your age ---> "))
-is_employed = bool(input("Are you currently employed (True/False) --->  "))
+is_employed = input("Are you currently employed (True/False) ---> ") == "True"
 credit_score = int(input("Credit score --> "))
 annual_income = float(input("What is your annual salary --> "))
-has_collateral = bool(input("Do you have any collateral (True/False) ---> "))
+has_collateral = input("Do you have any collateral (True/False) ---> ") == "True"
 
 base_rate = 0.0
 
 #BASELINE REQUIREMENT
-if age >= 21 and is_employed == True:
+if age >= 21 and is_employed:
     print("---- Applicant passed baseline requirement ----")
 
-    #TIER1
+    #TIER 1
     if credit_score >= 750:
         print("\t\tYou have a high credit score")
         base_rate = 5.0
@@ -27,12 +27,12 @@ if age >= 21 and is_employed == True:
         else:
             print("\t\tYour interest rate is", base_rate, "%")
 
-    #TIER2
+    #TIER 2
     elif credit_score >= 600 and credit_score < 750:
         print("\t\tYou have a fair credit score")
         base_rate = 8.0
 
-        if has_collateral == True:
+        if has_collateral:
             base_rate = 7.0
             print("\t\tYou have collateral")
             print("\t\tYour interest rate is", base_rate, "%")
@@ -45,10 +45,10 @@ if age >= 21 and is_employed == True:
         else:
             print("\t\tYour interest rate is", base_rate, "%")
 
-    #TIER3
+    #TIER 3
     else:
         print("\t\tRejected: Credit score too low")
 
 #FAILED BASELINE
 else:
-    print("\t\tRejected: Fails baseline criteria")
+    print("\t\t----Rejected: Fails baseline criteria----")
